e91f50aba9362f7da7545eaaf257d868d56dfc7a
Javeedkhan8 <javeedkhanjohnbasha8@gmail.com>
2025-03-20 19:10:42 +0530

    backend

--- Education_Coaching_CRM_project/backend/socket/chatSocket.js
+++ Education_Coaching_CRM_project/backend/socket/chatSocket.js
@@ -3,7 +3,7 @@ const { Server } = require("socket.io");
 const initSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5174",
+            origin: "https://lovely-moxie-b11887.netlify.app",
             methods: ["GET", "POST"],
             credentials: true,
         },
